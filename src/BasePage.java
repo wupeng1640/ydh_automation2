2ef564aeb58523c8ea78f7b60348a97c622a14b9
wupeng1640 <164553538@qq.com>
2016-03-08 09:16:18 +0800

    2016-3-8 提交整体的框架

--- src/BasePage.java
+++ src/BasePage.java
@@ -2,163 +2,105 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.server.handler.SubmitElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ThreadGuard;
+
+import javax.xml.ws.WebEndpoint;
+import java.security.PrivateKey;
 
 /**
- * Created by Administrator on 2016/1/29.
+ * Created by Administrator on 2016/3/7.
  */
 public class BasePage {
-  //  private static WebDriver driver;
+    private static WebDriver driver;
 
-//    public static WebDriver get(String url) {
-//        driver = new FirefoxDriver();
-//        driver.get(url);
-//        return driver;
-//    }
-//
-//    public static void quit() {
-//        driver.quit();
-//    }
-    public static void  main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","D:\\WebDriverServer\\chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-       // WebDriver driver=new FirefoxDriver();
+ /*   public static WebDriver driver() {
+        return driver;
+    }*/
 
-      //  BaiduIndexPage baidu=new BaiduIndexPage(driver);
-       // baidu.searchFor("Ë∏èÊ≠å");
-        LoginPage loginPage=new LoginPage(driver);
-        loginPage.loginydh("310005098","123456");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // loginPage.enroll();
-       // loginPage.FindPassword();
-        AdminFirstPage adminFirstPage=new AdminFirstPage(driver);
-        adminFirstPage.enProduct();
-        System.out.println("sadasd");
-    }
-}
-
-
-class BaiduIndexPage {
-    private WebDriver driver;
-    private final String url = "http://www.baidu.com";
-
-    public BaiduIndexPage(WebDriver driver) {
-        this.driver = driver;
-        driver.get(url);
+    public BasePage() {
+        System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+        driver = new ChromeDriver();
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(id = "kw")
-    private WebElement searchField;
-
-    @FindBy(id = "su")
-    private WebElement baidSearchButton;
-
-    public void searchFor(String term) {
-        searchField.clear();
-        searchField.sendKeys(term);
-        baidSearchButton.submit();
+    public void close() {
+        driver.close();
     }
 }
-//class Basepage{
-//   // private  WebDriver driver;
-//    public WebDriver driver;
-//
-//    public Basepage(WebDriver driver){
-//        this.driver=driver;
-//    }
-//}
-class LoginPage{
-    @FindBy(id="username")
-    private WebElement Username;
-    @FindBy(id="password")
-    private  WebElement Password;
-    @FindBy(id="btn-submit")
-    private  WebElement Submit;
-    @FindBy(linkText = "Á´ãÂç≥Ê≥®ÂÜå")
-    private WebElement Enroll;
-    @FindBy(linkText = "ÂøòËÆ∞ÂØÜÁ†ÅÔºü")
-    private WebElement Forgetpassword;
+
+class LoginPage  {
     private WebDriver driver;
-    private final String url = "https://sso.dinghuo123.com/login?service=ydh-web";
-    public LoginPage(WebDriver driver){
+    public LoginPage(WebDriver driver) {
         this.driver=driver;
-        driver.get(url);
-        PageFactory.initElements(driver,this);
+
+        PageFactory.initElements(driver, this);
     }
-    public void loginydh(String username,String password){
-        Username.clear();
-        Username.sendKeys(username);
-        Password.sendKeys(password);
-        Submit.click();
-        System.out.println(driver.getTitle());
+
+    public void close() {
+        driver.close();
+    }
+    public  WebDriver driver() {
+        return driver;
+    }
+    private WebElement username;
+    private WebElement password;
+    @FindBy(id="btn-submit")
+    private WebElement btn_submit;
+    @FindBy(xpath = "//*[@id=\"myForm\"]/div/div[4]/div/a")
+    private WebElement findPassworld;
+    @FindBy(xpath = "//*[@id=\"myForm\"]/div/div[7]/div/div/a")
+    private WebElement enroll;
+
+    public void get() {
+        driver.get("https://sso.dinghuo123.com/login?service=ydh-web");
+    }
+    public void login(String UserName, String PassWorld) {
+        username.clear();
+        username.sendKeys(UserName);
+        password.clear();
+        password.sendKeys(PassWorld);
+        btn_submit.click();
+        String tt=driver.getTitle();
+        System.out.println(tt);
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(driver.getTitle());
+        String  title=driver.getTitle();
+        System.out.println(title);
+        if(title.equals("Œ“µƒπ§◊˜Ã®"))
+            System.out.println("µ«¬º≥…π¶¡À");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public void enroll(){
-        Enroll.click();
-        System.out.println(driver.getTitle());
-    }
-    public void FindPassword(){
-        Forgetpassword.click();
-        System.out.println(driver.getTitle());
+    public EnrollPage linkPage(){
+        enroll.click();
+        EnrollPage enrollPage=new EnrollPage(driver);
+        return  enrollPage;
     }
 }
-class AdminFirstPage{
-    @FindBy(xpath = "//*[@id='order']/a")
-    private WebElement Order;
-    @FindBy(xpath = "//*[@id='product']/a")
-    private  WebElement Product;
-    @FindBy(xpath = "//*[@id='product']/div/ul/li[1]/ul/li[1]/a")
-    private WebElement ProductList;
-    @FindBy(xpath = "//*[@id='customer']/a")
-    private  WebElement Customer;
-    @FindBy(xpath = "//*[@id='pay']/a")
-    private  WebElement Pay;
-    @FindBy(xpath = "//*[@id='message']/a")
-    private  WebElement Message;
-    @FindBy(xpath = "//*[@id='report']/a")
-    private  WebElement  Report;
-    @FindBy(className = "info")
-    private WebElement Information;
-    @FindBy(className = "user menu1")
-    private WebElement UserMenul;
-    @FindBy(className = "setting menu1")
-    private  WebElement SettingMenu1;
-    @FindBy(className = "headerService menu1")
-    private WebElement HeaderServicMenu1;
+class EnrollPage {
     private WebDriver driver;
-    public AdminFirstPage(WebDriver driver){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
+    public EnrollPage(WebDriver driver) {
+        //this.driver=driver;
+       // System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+       // driver = new ChromeDriver();
+        PageFactory.initElements(driver, this);
     }
-   public void moveMouse(WebDriver driver,WebElement webElement){
-       Actions ac=new Actions(driver);
-       ac.moveToElement(webElement).perform();
-   }
-  public void enProduct(){
-      //this.moveMouse(this.driver,Product);
-      Actions actions=new Actions(this.driver);
-      actions.moveToElement(Product).perform();
-      ProductList.click();
 
-  }
+    public void close() {
+        driver.close();
+    }
+    @FindBy(id="username")
+    private WebElement username1;
+    public void cc(){
+        username1.sendKeys("18720091224");
+    }
+
+
 }
-
-
-
-
---
+++ src/JunitTest.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Administrator on 2016/3/7.
+ */
+public class JunitTest {
+    private WebDriver driver;
+    //BasePage basePage=new BasePage();
+    LoginPage loginPage;
+    EnrollPage enrollPage;
+
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+        driver = new ChromeDriver();
+        loginPage=new LoginPage(driver);
+       // loginPage.get();
+       // loginPage.login("31000598","123456");
+
+    }
+     @Test
+    public void test(){
+         loginPage.get();
+         enrollPage= loginPage.linkPage();
+         enrollPage.cc();
+
+         //loginPage.login("310005098","123456");
+     }
+    @After
+    public void tearDown(){
+        loginPage.close();
+    }
+}
